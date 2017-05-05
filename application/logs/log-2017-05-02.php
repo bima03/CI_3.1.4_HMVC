a0f06e58ea897720fe715ea13e109fa2158f6601
bima03 <bimarossoneri@gmail.com>
2017-05-05 09:13:55 +0700

    version.1

---
+++ application/logs/log-2017-05-02.php
@@ -0,0 +1,152 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-05-02 17:51:22 --> Config Class Initialized
+INFO - 2017-05-02 17:51:22 --> Hooks Class Initialized
+DEBUG - 2017-05-02 17:51:22 --> UTF-8 Support Enabled
+INFO - 2017-05-02 17:51:22 --> Utf8 Class Initialized
+INFO - 2017-05-02 17:51:22 --> URI Class Initialized
+DEBUG - 2017-05-02 17:51:22 --> No URI present. Default controller set.
+INFO - 2017-05-02 17:51:22 --> Router Class Initialized
+INFO - 2017-05-02 17:51:22 --> Output Class Initialized
+INFO - 2017-05-02 17:51:22 --> Security Class Initialized
+DEBUG - 2017-05-02 17:51:22 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-05-02 17:51:22 --> Input Class Initialized
+INFO - 2017-05-02 17:51:22 --> Language Class Initialized
+INFO - 2017-05-02 17:51:22 --> Language Class Initialized
+INFO - 2017-05-02 17:51:22 --> Config Class Initialized
+INFO - 2017-05-02 17:51:22 --> Loader Class Initialized
+INFO - 2017-05-02 17:51:22 --> Controller Class Initialized
+DEBUG - 2017-05-02 17:51:22 --> Welcome MX_Controller Initialized
+DEBUG - 2017-05-02 17:51:22 --> File loaded: C:\xampp\htdocs\ci3\application\modules/Welcome/views/welcome_message.php
+INFO - 2017-05-02 17:51:22 --> Final output sent to browser
+DEBUG - 2017-05-02 17:51:22 --> Total execution time: 0.1160
+INFO - 2017-05-02 17:53:27 --> Config Class Initialized
+INFO - 2017-05-02 17:53:27 --> Hooks Class Initialized
+DEBUG - 2017-05-02 17:53:27 --> UTF-8 Support Enabled
+INFO - 2017-05-02 17:53:27 --> Utf8 Class Initialized
+INFO - 2017-05-02 17:53:27 --> URI Class Initialized
+DEBUG - 2017-05-02 17:53:27 --> No URI present. Default controller set.
+INFO - 2017-05-02 17:53:27 --> Router Class Initialized
+INFO - 2017-05-02 17:53:27 --> Output Class Initialized
+INFO - 2017-05-02 17:53:27 --> Security Class Initialized
+DEBUG - 2017-05-02 17:53:27 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-05-02 17:53:27 --> Input Class Initialized
+INFO - 2017-05-02 17:53:27 --> Language Class Initialized
+INFO - 2017-05-02 17:53:27 --> Language Class Initialized
+INFO - 2017-05-02 17:53:27 --> Config Class Initialized
+INFO - 2017-05-02 17:53:27 --> Loader Class Initialized
+INFO - 2017-05-02 17:53:27 --> Controller Class Initialized
+DEBUG - 2017-05-02 17:53:27 --> Welcome MX_Controller Initialized
+DEBUG - 2017-05-02 17:53:27 --> File loaded: C:\xampp\htdocs\ci3\application\modules/Welcome/views/welcome_message.php
+INFO - 2017-05-02 17:53:27 --> Final output sent to browser
+DEBUG - 2017-05-02 17:53:27 --> Total execution time: 0.0430
+INFO - 2017-05-02 17:53:35 --> Config Class Initialized
+INFO - 2017-05-02 17:53:35 --> Hooks Class Initialized
+DEBUG - 2017-05-02 17:53:35 --> UTF-8 Support Enabled
+INFO - 2017-05-02 17:53:35 --> Utf8 Class Initialized
+INFO - 2017-05-02 17:53:35 --> URI Class Initialized
+INFO - 2017-05-02 17:53:35 --> Router Class Initialized
+INFO - 2017-05-02 17:53:35 --> Output Class Initialized
+INFO - 2017-05-02 17:53:35 --> Security Class Initialized
+DEBUG - 2017-05-02 17:53:35 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-05-02 17:53:35 --> Input Class Initialized
+INFO - 2017-05-02 17:53:35 --> Language Class Initialized
+INFO - 2017-05-02 17:53:35 --> Language Class Initialized
+INFO - 2017-05-02 17:53:35 --> Config Class Initialized
+INFO - 2017-05-02 17:53:35 --> Loader Class Initialized
+INFO - 2017-05-02 17:53:35 --> Controller Class Initialized
+DEBUG - 2017-05-02 17:53:35 --> Track MX_Controller Initialized
+INFO - 2017-05-02 17:53:35 --> Final output sent to browser
+DEBUG - 2017-05-02 17:53:35 --> Total execution time: 0.0486
+INFO - 2017-05-02 17:55:03 --> Config Class Initialized
+INFO - 2017-05-02 17:55:03 --> Hooks Class Initialized
+DEBUG - 2017-05-02 17:55:03 --> UTF-8 Support Enabled
+INFO - 2017-05-02 17:55:03 --> Utf8 Class Initialized
+INFO - 2017-05-02 17:55:03 --> URI Class Initialized
+INFO - 2017-05-02 17:55:03 --> Router Class Initialized
+INFO - 2017-05-02 17:55:03 --> Output Class Initialized
+INFO - 2017-05-02 17:55:03 --> Security Class Initialized
+DEBUG - 2017-05-02 17:55:03 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-05-02 17:55:03 --> Input Class Initialized
+INFO - 2017-05-02 17:55:03 --> Language Class Initialized
+INFO - 2017-05-02 17:55:03 --> Language Class Initialized
+INFO - 2017-05-02 17:55:03 --> Config Class Initialized
+INFO - 2017-05-02 17:55:03 --> Loader Class Initialized
+INFO - 2017-05-02 17:55:03 --> Controller Class Initialized
+DEBUG - 2017-05-02 17:55:03 --> Track MX_Controller Initialized
+INFO - 2017-05-02 17:55:03 --> Final output sent to browser
+DEBUG - 2017-05-02 17:55:03 --> Total execution time: 0.0816
+INFO - 2017-05-02 17:56:00 --> Config Class Initialized
+INFO - 2017-05-02 17:56:00 --> Hooks Class Initialized
+DEBUG - 2017-05-02 17:56:00 --> UTF-8 Support Enabled
+INFO - 2017-05-02 17:56:00 --> Utf8 Class Initialized
+INFO - 2017-05-02 17:56:00 --> URI Class Initialized
+INFO - 2017-05-02 17:56:00 --> Router Class Initialized
+INFO - 2017-05-02 17:56:00 --> Output Class Initialized
+INFO - 2017-05-02 17:56:00 --> Security Class Initialized
+DEBUG - 2017-05-02 17:56:00 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-05-02 17:56:00 --> Input Class Initialized
+INFO - 2017-05-02 17:56:00 --> Language Class Initialized
+INFO - 2017-05-02 17:56:00 --> Language Class Initialized
+INFO - 2017-05-02 17:56:00 --> Config Class Initialized
+INFO - 2017-05-02 17:56:00 --> Loader Class Initialized
+INFO - 2017-05-02 17:56:00 --> Controller Class Initialized
+DEBUG - 2017-05-02 17:56:00 --> Track MX_Controller Initialized
+INFO - 2017-05-02 17:56:00 --> Final output sent to browser
+DEBUG - 2017-05-02 17:56:00 --> Total execution time: 0.0410
+INFO - 2017-05-02 17:56:01 --> Config Class Initialized
+INFO - 2017-05-02 17:56:01 --> Hooks Class Initialized
+DEBUG - 2017-05-02 17:56:01 --> UTF-8 Support Enabled
+INFO - 2017-05-02 17:56:01 --> Utf8 Class Initialized
+INFO - 2017-05-02 17:56:01 --> URI Class Initialized
+INFO - 2017-05-02 17:56:01 --> Router Class Initialized
+INFO - 2017-05-02 17:56:01 --> Output Class Initialized
+INFO - 2017-05-02 17:56:01 --> Security Class Initialized
+DEBUG - 2017-05-02 17:56:01 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-05-02 17:56:01 --> Input Class Initialized
+INFO - 2017-05-02 17:56:01 --> Language Class Initialized
+INFO - 2017-05-02 17:56:01 --> Language Class Initialized
+INFO - 2017-05-02 17:56:01 --> Config Class Initialized
+INFO - 2017-05-02 17:56:01 --> Loader Class Initialized
+INFO - 2017-05-02 17:56:01 --> Controller Class Initialized
+DEBUG - 2017-05-02 17:56:01 --> Track MX_Controller Initialized
+INFO - 2017-05-02 17:56:01 --> Final output sent to browser
+DEBUG - 2017-05-02 17:56:01 --> Total execution time: 0.0506
+INFO - 2017-05-02 23:17:05 --> Config Class Initialized
+INFO - 2017-05-02 23:17:05 --> Hooks Class Initialized
+DEBUG - 2017-05-02 23:17:05 --> UTF-8 Support Enabled
+INFO - 2017-05-02 23:17:05 --> Utf8 Class Initialized
+INFO - 2017-05-02 23:17:05 --> URI Class Initialized
+DEBUG - 2017-05-02 23:17:05 --> No URI present. Default controller set.
+INFO - 2017-05-02 23:17:05 --> Router Class Initialized
+INFO - 2017-05-02 23:17:05 --> Output Class Initialized
+INFO - 2017-05-02 23:17:05 --> Security Class Initialized
+DEBUG - 2017-05-02 23:17:05 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-05-02 23:17:05 --> Input Class Initialized
+INFO - 2017-05-02 23:17:05 --> Language Class Initialized
+INFO - 2017-05-02 23:17:05 --> Language Class Initialized
+INFO - 2017-05-02 23:17:05 --> Config Class Initialized
+INFO - 2017-05-02 23:17:05 --> Loader Class Initialized
+INFO - 2017-05-02 23:17:05 --> Controller Class Initialized
+DEBUG - 2017-05-02 23:17:05 --> Welcome MX_Controller Initialized
+DEBUG - 2017-05-02 23:17:05 --> File loaded: C:\xampp\htdocs\ci3\application\modules/Welcome/views/welcome_message.php
+INFO - 2017-05-02 23:17:05 --> Final output sent to browser
+DEBUG - 2017-05-02 23:17:05 --> Total execution time: 0.2140
+INFO - 2017-05-02 23:19:12 --> Config Class Initialized
+INFO - 2017-05-02 23:19:12 --> Hooks Class Initialized
+DEBUG - 2017-05-02 23:19:12 --> UTF-8 Support Enabled
+INFO - 2017-05-02 23:19:12 --> Utf8 Class Initialized
+INFO - 2017-05-02 23:19:12 --> URI Class Initialized
+INFO - 2017-05-02 23:19:12 --> Router Class Initialized
+INFO - 2017-05-02 23:19:12 --> Output Class Initialized
+INFO - 2017-05-02 23:19:12 --> Security Class Initialized
+DEBUG - 2017-05-02 23:19:12 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-05-02 23:19:12 --> Input Class Initialized
+INFO - 2017-05-02 23:19:12 --> Language Class Initialized
+INFO - 2017-05-02 23:19:12 --> Language Class Initialized
+INFO - 2017-05-02 23:19:12 --> Config Class Initialized
+INFO - 2017-05-02 23:19:12 --> Loader Class Initialized
+INFO - 2017-05-02 23:19:12 --> Controller Class Initialized
+DEBUG - 2017-05-02 23:19:13 --> Track MX_Controller Initialized
+INFO - 2017-05-02 23:19:13 --> Final output sent to browser
+DEBUG - 2017-05-02 23:19:13 --> Total execution time: 0.1940
